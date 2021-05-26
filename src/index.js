588c718bde37067e83c694f4b539707cb137460d
Charlie <peabody00@gmail.com>
2021-05-26 13:23:13 -0500

    commit tree e93c38023b2bbe6f3ac505042480cae88b8c4823

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchImages()
+    // fetchDogBreeds()
+    // getDropDown().addEventListener('change', filterBreeds)
+})
+
+function fetchImages() {
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => data.message.forEach(image => displayImage(image)))
+};
+
+function displayImage(image) {
+    const imageItem = document.createElement('img')
+    imageItem.src = image
+    getDogImageContainer().append(imageItem)
+}
+
+// functions to grab DOM nodes
+function getDogImageContainer() {
+    return document.getElementById('dog-image-container')
+}
